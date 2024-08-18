cc22f1fd446b3cae94ffa80a24a5bc23a63f91c5
rodrigo <martinqueve60@gmail.com>
2024-08-18 19:42:37 -0300

    Square state handling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import {useState} from 'react';
 
 export default function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
+    function handlePlay(value) {
+        let player = "X"
+        console.log('in board')
+        executePlay(value, player)
+    }
+    function executePlay(value, player) {
+        console.log('ok')
+        let nextSquares = [...squares];
+        nextSquares[value] = player;
+        setSquares(nextSquares)
+    }
+
     return (
     <div className="boardContainer">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={0} handlePlayProp={handlePlay} squares={squares}/>
+        <Square value={1} handlePlayProp={handlePlay} squares={squares}/>
+        <Square value={2} handlePlayProp={handlePlay} squares={squares}/>
        
-        <Square />
-        <Square />
-        <Square />
+        <Square value={3} handlePlayProp={handlePlay} squares={squares}/>
+        <Square value={4} handlePlayProp={handlePlay} squares={squares}/>
+        <Square value={5} handlePlayProp={handlePlay} squares={squares}/>
        
-        <Square />
-        <Square />
-        <Square />
+        <Square value={6} handlePlayProp={handlePlay} squares={squares}/>
+        <Square value={7} handlePlayProp={handlePlay} squares={squares}/>
+        <Square value={8} handlePlayProp={handlePlay} squares={squares}/>
     </div>
 )   
 }
 
-function Square() {
-    const [value, setValue] = useState(null);
-
+function Square({value, handlePlayProp, squares}) {
     function handleClick() {
-        setValue("X")
-        }
+        console.log('in square')
+        handlePlayProp(value)
+    }
 
     return (
         <button className="square" onClick={handleClick}>
-            {value}
+            {squares[value]}
         </button>
     )
 }
