7de5e69e590a1b3837bf4c353071f4768677c576
rodrigo <martinqueve60@gmail.com>
2024-08-19 15:59:44 -0300

    status text

--- src/App.js
+++ src/App.js
@@ -16,20 +16,31 @@ export default function Board() {
         setSquares(nextSquares)
     }
 
+    let text;
+    if (calculateWinner(squares)){
+        text = `Winner is ${calculateWinner(squares)}`
+    }
+    else {
+        text = `Next player is: ${xIsNext ? 'X' : 'O'}`
+    }
+
     return (
-    <div className="boardContainer">
-        <Square value={squares[0]} onClickProp={()=> {handleClick(0)}}/>
-        <Square value={squares[1]} onClickProp={()=> {handleClick(1)}}/>
-        <Square value={squares[2]} onClickProp={()=> {handleClick(2)}}/>
-       
-        <Square value={squares[3]} onClickProp={()=> {handleClick(3)}}/>
-        <Square value={squares[4]} onClickProp={()=> {handleClick(4)}}/>
-        <Square value={squares[5]} onClickProp={()=> {handleClick(5)}}/>
-       
-        <Square value={squares[6]} onClickProp={()=> {handleClick(6)}}/>
-        <Square value={squares[7]} onClickProp={()=> {handleClick(7)}}/>
-        <Square value={squares[8]} onClickProp={()=> {handleClick(8)}}/>
-    </div>
+        <>
+            <div className="status">{text}</div>
+            <div className="boardContainer">
+                <Square value={squares[0]} onClickProp={()=> {handleClick(0)}}/>
+                <Square value={squares[1]} onClickProp={()=> {handleClick(1)}}/>
+                <Square value={squares[2]} onClickProp={()=> {handleClick(2)}}/>
+            
+                <Square value={squares[3]} onClickProp={()=> {handleClick(3)}}/>
+                <Square value={squares[4]} onClickProp={()=> {handleClick(4)}}/>
+                <Square value={squares[5]} onClickProp={()=> {handleClick(5)}}/>
+            
+                <Square value={squares[6]} onClickProp={()=> {handleClick(6)}}/>
+                <Square value={squares[7]} onClickProp={()=> {handleClick(7)}}/>
+                <Square value={squares[8]} onClickProp={()=> {handleClick(8)}}/>
+            </div>
+    </>
 )   
 }
 
