223b034252c7740f0f834a4163206caea7d30a12
rodrigo <martinqueve60@gmail.com>
2024-08-18 23:22:51 -0300

    click event handler

--- src/App.js
+++ src/App.js
@@ -3,31 +3,32 @@ import {useState} from 'react';
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
 
+    function handleClick(i) {
+        let nextSquares = [...squares]
+        nextSquares[i] = "X"
+        setSquares(nextSquares)
+    }
+
     return (
     <div className="boardContainer">
-        <Square value={squares[0]}/>
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+        <Square value={squares[0]} onClickProp={()=> {handleClick(0)}}/>
+        <Square value={squares[1]} onClickProp={()=> {handleClick(1)}}/>
+        <Square value={squares[2]} onClickProp={()=> {handleClick(2)}}/>
        
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} onClickProp={()=> {handleClick(3)}}/>
+        <Square value={squares[4]} onClickProp={()=> {handleClick(4)}}/>
+        <Square value={squares[5]} onClickProp={()=> {handleClick(5)}}/>
        
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} onClickProp={()=> {handleClick(6)}}/>
+        <Square value={squares[7]} onClickProp={()=> {handleClick(7)}}/>
+        <Square value={squares[8]} onClickProp={()=> {handleClick(8)}}/>
     </div>
 )   
 }
 
-function Square({value}) {
-
-    function handleClick() {
-        setValue("X")
-    }
-
+function Square({value, onClickProp}) {
     return (
-        <button className="square" onClick={handleClick}>
+        <button className="square" onClick={onClickProp}>
             {value}
         </button>
     )
